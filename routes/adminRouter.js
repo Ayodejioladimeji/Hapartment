ef3cf6492089da6b3c1da11ce920edde5869b5d0
Ayodejioladimeji <layobright11@gmail.com>
2023-03-18 05:46:25 +0100

    added updates

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -12,11 +12,11 @@ router.post("/create-admin", adminCtrl.createAdmin);
 router.post("/login-admin", adminCtrl.loginAdmin);
 
 // patch request
-// router.patch("/admin-updateuser", auth, adminCtrl.updateUser);
-router.patch("/approve-agent/:id", auth, adminCtrl.approveAgent);
-router.patch("/decline-agent/:id", auth, adminCtrl.declineAgent);
-router.patch("/approve-listing/:id", auth, adminCtrl.approveListing);
-router.patch("/decline-listing/:id", auth, adminCtrl.declineListing);
+// router.patch("/admin-updateuser", adminCtrl.updateUser);
+router.patch("/approve-agent/:id", adminCtrl.approveAgent);
+router.patch("/decline-agent/:id", adminCtrl.declineAgent);
+router.patch("/approve-listing/:id", adminCtrl.approveListing);
+router.patch("/decline-listing/:id", adminCtrl.declineListing);
 
 // get request
 router.get("/all-users", adminCtrl.getAllUsers);
